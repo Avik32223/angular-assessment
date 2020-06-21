4d3275a835d52b1199b8e295288b68ee20afbff5
Avik <sayavik@gmail.com>
2020-06-21 08:38:06 +0530

    Added navigation component to be used in all showcases

--- src/app/input-output-timer/input-output-timer.module.ts
+++ src/app/input-output-timer/input-output-timer.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
-import { InputOutputTimerRoutingModule } from './input-output-timer-routing.module';
-import { InputOutputTimerComponent } from './input-output-timer.component';
-import { DisplayComponent } from './display/display.component';
-import { FormsComponent } from './forms/forms.component';
-import { LoggerComponent } from './logger/logger.component';
-import { CounterStatusComponent } from './counter-status/counter-status.component';
+import { InputOutputTimerRoutingModule } from "./input-output-timer-routing.module";
+import { InputOutputTimerComponent } from "./input-output-timer.component";
+import { DisplayComponent } from "./display/display.component";
+import { FormsComponent } from "./forms/forms.component";
+import { LoggerComponent } from "./logger/logger.component";
+import { CounterStatusComponent } from "./counter-status/counter-status.component";
+import { NavigationModule } from "../navigation/navigation.module";
 
 @NgModule({
-  imports: [CommonModule, InputOutputTimerRoutingModule, FormsModule],
+  imports: [
+    CommonModule,
+    InputOutputTimerRoutingModule,
+    FormsModule,
+    NavigationModule,
+  ],
   declarations: [
     InputOutputTimerComponent,
     DisplayComponent,
--- src/app/service-timer/service-timer.module.ts
+++ src/app/service-timer/service-timer.module.ts
@@ -1,15 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { ServiceTimerRoutingModule } from './service-timer-routing.module';
-import { ServiceTimerComponent } from './service-timer.component';
-import { CounterStatusComponent } from './counter-status/counter-status.component';
-import { DisplayComponent } from './display/display.component';
-import { FormsComponent } from './forms/forms.component';
-import { LoggerComponent } from './logger/logger.component';
+import { ServiceTimerRoutingModule } from "./service-timer-routing.module";
+import { ServiceTimerComponent } from "./service-timer.component";
+import { CounterStatusComponent } from "./counter-status/counter-status.component";
+import { DisplayComponent } from "./display/display.component";
+import { FormsComponent } from "./forms/forms.component";
+import { LoggerComponent } from "./logger/logger.component";
+import { NavigationModule } from "../navigation/navigation.module";
 
 @NgModule({
-  imports: [CommonModule, ServiceTimerRoutingModule],
-  declarations: [ServiceTimerComponent, CounterStatusComponent, DisplayComponent, FormsComponent, LoggerComponent],
+  imports: [CommonModule, ServiceTimerRoutingModule, NavigationModule],
+  declarations: [
+    ServiceTimerComponent,
+    CounterStatusComponent,
+    DisplayComponent,
+    FormsComponent,
+    LoggerComponent,
+  ],
 })
 export class ServiceTimerModule {}
